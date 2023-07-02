2262f6910ebbdfd5968eaa5e5ab8d6c7da93b8d2
PARIK SHARMA <76546200+Parik4988@users.noreply.github.com>
2023-07-02 12:05:43 +0530

    package update

---
+++ src/utils/audio_category.ts
@@ -0,0 +1,13 @@
+export const categories = [
+  "Arts",
+  "Business",
+  "Education",
+  "Entertainment",
+  "Kids & Family",
+  "Music",
+  "Science",
+  "Tech",
+  "Others",
+];
+
+export type categoriesTypes = "Arts"|"Business"|"Education"|"Entertainment"|"Kids & Family"|"Music"|"Science"|"Tech"|"Others";
